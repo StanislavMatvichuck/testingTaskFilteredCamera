09e56617b237a54ce4cc9e604fb4120fedf777c1
StanislavMck2120 <stanislavmat@rozdoum.com>
2023-07-16 14:17:33 +0300

    initial commit

---
+++ FilteredCamera/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FilteredCamera
+//
+//  Created by Stanislav Matvichuck on 16.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
