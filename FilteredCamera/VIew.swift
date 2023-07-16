45d0377ba1041ec7baf45af3f76647fee7db0713
StanislavMck2120 <stanislavmat@rozdoum.com>
2023-07-16 15:38:03 +0300

    basic capture session setup

--- FilteredCamera/VIew.swift
+++ FilteredCamera/VIew.swift
@@ -5,9 +5,18 @@
 //  Created by Stanislav Matvichuck on 16.07.2023.
 //
 
+import AVFoundation
 import UIKit
 
 final class View: UIView {
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+
+    private var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+
     init() {
         super.init(frame: .zero)
         configureAppearance()
@@ -17,6 +26,13 @@ final class View: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - Public
+    func allowCapturing(for session: AVCaptureSession) {
+        videoPreviewLayer.session = session
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+    }
+
+    // MARK: - Private
     private func configureAppearance() {
         backgroundColor = .red
     }
--- FilteredCamera/ViewController.swift
+++ FilteredCamera/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Stanislav Matvichuck on 16.07.2023.
 //
 
+import AVFoundation
 import UIKit
 
 final class ViewController: UIViewController {
@@ -13,5 +14,67 @@ final class ViewController: UIViewController {
     override func loadView() { view = rootView }
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        configureCapturePermissions()
+    }
+
+    private func configureCapturePermissions() {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+
+        switch status {
+        case .denied:
+            displayDeniedAlert()
+        case .authorized:
+            configureCaptureSession()
+        default:
+            requestCaptureAuthorization()
+        }
+    }
+
+    private func requestCaptureAuthorization() {
+        AVCaptureDevice.requestAccess(for: .video) { [weak self] accessGranted in
+            guard accessGranted, let self else { return }
+            self.configureCaptureSession()
+        }
+    }
+
+    private func configureCaptureSession() {
+        let output = AVCaptureVideoDataOutput()
+        let videoDevice = AVCaptureDevice.default(
+            .builtInWideAngleCamera,
+            for: .video,
+            position: .unspecified
+        )
+
+        let session = AVCaptureSession()
+        session.beginConfiguration()
+
+        guard
+            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!),
+            session.canAddInput(videoDeviceInput),
+            session.canAddOutput(output)
+        else { return }
+
+        session.addInput(videoDeviceInput)
+        session.addOutput(output)
+        session.sessionPreset = .high
+        session.commitConfiguration()
+
+        rootView.allowCapturing(for: session)
+
+        session.startRunning()
+    }
+
+    private func displayDeniedAlert() {
+        let alert = UIAlertController(
+            title: "Camera access denied",
+            message: "Please allow camera usage in settings",
+            preferredStyle: .alert
+        )
+
+        present(alert, animated: true)
     }
 }
